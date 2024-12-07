8e1bd1e7faeefc20c97b8d39e2f2daeecd44317b
Nikitin-m <nikmnik20@gmail.com>
2024-12-07 11:30:37 +0100

    file generator

---
+++ FileGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace FileDataSorter;
+
+internal sealed class FileGenerator
+{
+    private readonly Random _random = new();
+    private readonly string[] _words;
+
+    public FileGenerator()
+    {       
+        _words = Enumerable.Range(1, 100000)
+            .Select(_ =>
+            {
+                var wordLength = Enumerable.Range(5, _random.Next(20, 100));
+                var charArray = wordLength.Select(x => (char)_random.Next('A', 'Z')).ToArray();
+                var str = new string(charArray);
+                return str;
+            }).ToArray();
+    }
+    public string Generate(long sizeBytes)
+    {
+        var fileName = $"f_{sizeBytes}.txt";
+        var currentFileSize = 0L;
+        using var writer = new StreamWriter(fileName);
+        while (currentFileSize < sizeBytes)
+        {
+            var line = GetLine();
+            writer.WriteLine(line);
+            var currentLineSize = Encoding.UTF8.GetBytes(line + Environment.NewLine).LongLength;
+            currentFileSize += currentLineSize;
+        }
+        
+        return fileName;
+    }
+
+    private string GetLine()
+    {
+        return $"{GetNumber()}.{GetString()}";
+    }
+
+    private int GetNumber()
+    {
+        return _random.Next(0, int.MaxValue);
+    }
+
+    private string GetString()
+    {
+        return _words[_random.Next(0, _words.Length)];
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
+using FileDataSorter;
 
-Console.WriteLine("Hello, World!");
+var stopwatch = Stopwatch.StartNew();
+const long sizeInBytes = 1024 * 1024 * 1024; // 1gb
+var fileName = "f_1073741824.txt";//new FileGenerator().Generate(sizeInBytes * 20);
+stopwatch.Stop();
+Console.WriteLine($"{fileName} generation took {stopwatch.ElapsedMilliseconds} ms");
