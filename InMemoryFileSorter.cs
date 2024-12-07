936d2623a4c51573df867cd21b54419d7a93f1b4
Nikitin-m <nikmnik20@gmail.com>
2024-12-07 15:06:57 +0100

    external sorting init

---
+++ InMemoryFileSorter.cs
@@ -0,0 +1,38 @@
+﻿internal class InMemoryFileSorter
+{
+    public void Sort(string fileName)
+    {
+        var dict = new Dictionary<string, List<int>>();
+        using var reader = File.OpenText(fileName);
+        while (true)
+        {
+            var line = reader.ReadLine();
+            if (string.IsNullOrWhiteSpace(line))
+                break;
+
+            var span = line.AsSpan();
+            var index = span.IndexOf('.');
+            var number = int.Parse(span.Slice(0, index));
+            var str = span.Slice(index + 2).ToString();
+            
+            if (dict.TryGetValue(str, out var value))
+                value.Add(number);
+            else 
+                dict.Add(str, new List<int> { number });
+        }
+
+        using var writer = new StreamWriter($"sorted_{fileName}");
+        
+        foreach (var strNumber in dict.OrderBy(x=> x.Key))
+        {
+            var numbers = strNumber.Value.ToArray();
+            Array.Sort(numbers);
+            foreach (var number in numbers)
+            {
+                writer.Write(number);
+                writer.Write(". ");
+                writer.WriteLine(strNumber.Key);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,41 +15,20 @@ new InMemoryFileSorter().Sort(fileName);
 stopwatch.Stop();
 Console.WriteLine($"{fileName} sorting took {stopwatch.ElapsedMilliseconds} ms");
 
-internal class InMemoryFileSorter
+
+internal class FileSorter
 {
     public void Sort(string fileName)
     {
-        var dict = new Dictionary<string, List<int>>();
         using var reader = File.OpenText(fileName);
-        while (true)
+        for (;;)
         {
-            var line = reader.ReadLine();
-            if (string.IsNullOrWhiteSpace(line))
-                break;
-
-            var span = line.AsSpan();
-            var index = span.IndexOf('.');
-            var number = int.Parse(span.Slice(0, index));
-            var str = span.Slice(index + 2).ToString();
             
-            if (dict.TryGetValue(str, out var value))
-                value.Add(number);
-            else 
-                dict.Add(str, new List<int> { number });
         }
-
-        using var writer = new StreamWriter($"sorted_{fileName}");
         
-        foreach (var strNumber in dict.OrderBy(x=> x.Key))
-        {
-            var numbers = strNumber.Value.ToArray();
-            Array.Sort(numbers);
-            foreach (var number in numbers)
-            {
-                writer.Write(number);
-                writer.Write(". ");
-                writer.WriteLine(strNumber.Key);
-            }
-        }
+        //todo
+        //split into files
+        //sort files
+        //merge into new one
     }
 }
